3f849ece424dc38346af22ea8917aa8c939d6ed4
keivan-shah <keivanshahh@gmail.com>
2017-01-26 00:05:54 +0530

    Added TheBot

---
+++ TheBot/GardenerBot.java
@@ -0,0 +1,353 @@
+package TheBot;
+
+import battlecode.common.*;
+
+import static TheBot.Messaging.*;
+
+public class GardenerBot extends BaseBot
+{
+    private static final Direction HOLE_TOWARDS_ENEMY = here.directionTo(closetInitalEnemyArchonLocation());
+    private static final int ROUND_SPAWNED = rc.getRoundNum();
+    private static Direction bounce = here.directionTo(centerOfAllInitialArchons);
+
+    private static Direction unitSpawnDirection = HOLE_TOWARDS_ENEMY;
+    private static Direction[] treeSpawnDirections = null;
+
+    private static int treesBuiltByMe = 0;
+    private static boolean needExtraLumberjacks = false;
+    private static boolean earlySoldier = rc.getRoundNum() < 100;
+    private static boolean earlyLumberjack = rc.getRoundNum() < 100;
+    private static boolean tankGardener = Math.random() > 0.4 && rc.getRoundNum() > 450;
+
+    public static void runGardener() throws GameActionException
+    {
+        try
+        {
+            generateDirections();
+            if (!tankGardener)
+            {
+                here = rc.getLocation();
+                if(earlySoldier)
+                {
+                    int treesInPath = rc.senseNearbyTrees(here.add(HOLE_TOWARDS_ENEMY, myType.sensorRadius), -1, Team.NEUTRAL).length;
+                    if (treesInPath > 5)
+                    {
+                        earlyLumberjack = true;
+                        earlySoldier = false;
+                    }
+                }
+                MapLocation bestLocationSoFar = here;
+                int numTrees = howManyTreesCanBePlanted(here);
+                int maxSoFar = numTrees;
+                boolean brokeEarly = false;
+                while (numTrees < 4)
+                {
+
+                    int currentRound = rc.getRoundNum();
+                    if (currentRound - ROUND_SPAWNED > determineWanderDuration(currentRound))
+                    {
+                        if (rc.senseNearbyTrees(-1, Team.NEUTRAL).length >= 4)
+                            needExtraLumberjacks = true;
+                        brokeEarly = true;
+                        break;
+
+                    }
+                    else if (!rc.hasMoved())
+                    {
+                        wander();
+                        Clock.yield();
+                    }
+                    here = rc.getLocation();
+                    numTrees = howManyTreesCanBePlanted(here);
+                    if (numTrees > maxSoFar)
+                    {
+                        maxSoFar = numTrees;
+                        bestLocationSoFar = here;
+                    }
+
+                }
+                if (brokeEarly)
+                {
+                    if (numTrees < maxSoFar)
+                    {
+                        here = rc.getLocation();
+                        while (!here.isWithinDistance(bestLocationSoFar, myType.bodyRadius))
+                        {
+                            here = rc.getLocation();
+                            tryMove(here.directionTo(bestLocationSoFar));
+                            Clock.yield();
+                        }
+                    }
+                }
+
+            }
+        } catch (GameActionException e)
+        {
+            e.printStackTrace();
+        }
+
+        while (true)
+
+        {
+            try
+            {
+                visibleEnemies = rc.senseNearbyRobots(-1, them);
+                callForHelpIfInDangerAndDefend();
+                if (rc.getHealth() < RobotType.GARDENER.maxHealth / 10 && visibleEnemies.length > 0)
+                {
+                    rc.broadcast(NUM_GARDENERS_CHANNEL, (rc.readBroadcast(NUM_GARDENERS_CHANNEL) - 1));
+
+                }
+                here = rc.getLocation();
+                if (tankGardener)
+                {
+                    if (rc.getTeamBullets() > 400)
+                    {
+                        spawnInAnyDirectionPossible(RobotType.TANK);
+                    }
+                    wander();
+
+
+                }
+                else
+                {
+                    //rc.setIndicatorDot(here.add(dir, 2), 255, 0, 0);
+                    if (rc.readBroadcast(EARLY_GAME_SCOUT_SPAWNED_CHANNEL) == 0)
+                    {
+                        if (spawnInAnyDirectionPossible(RobotType.SCOUT))
+                        {
+                            //rc.broadcast(NUM_SCOUTS_CHANNEL, (rc.readBroadcast(NUM_SCOUTS_CHANNEL) + 1));
+                            rc.broadcast(EARLY_GAME_SCOUT_SPAWNED_CHANNEL, 1);
+                            Clock.yield();
+                        }
+                    }
+                    else
+                    {
+                        if(rc.getRoundNum() < 150 && rc.isBuildReady() && !needExtraLumberjacks)
+                        {
+                            spawnInAnyDirectionPossible(RobotType.SOLDIER);
+                        }
+                    }
+
+                    if (needExtraLumberjacks && rc.isBuildReady())
+                    {
+                        if (spawnInAnyDirectionPossible(RobotType.LUMBERJACK))
+                        {
+                            needExtraLumberjacks = false;
+                            Clock.yield();
+                        }
+
+                    }
+                    if (earlySoldier && rc.isBuildReady())
+                    {
+                        if (spawnInAnyDirectionPossible(RobotType.SOLDIER))
+                        {
+                            earlySoldier = false;
+                            Clock.yield();
+                        }
+                    }
+                    else
+                    {
+                        if(earlyLumberjack && rc.isBuildReady())
+                        {
+                            if(spawnInAnyDirectionPossible(RobotType.LUMBERJACK))
+                            {
+                                earlyLumberjack = false;
+                                Clock.yield();
+                            }
+                        }
+                    }
+
+
+                    //Baith gaya
+                    for (int i = treeSpawnDirections.length; i-- > 0; )
+                    {
+                        if (rc.hasTreeBuildRequirements())
+                        {
+                            if (rc.canPlantTree(treeSpawnDirections[i]))
+                            {
+                                rc.plantTree(treeSpawnDirections[i]);
+                                treesBuiltByMe++;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (needScouts())
+                    {
+                        if (rc.hasRobotBuildRequirements(RobotType.SCOUT))
+                        {
+                            if (rc.canBuildRobot(RobotType.SCOUT, unitSpawnDirection))
+                            {
+                                rc.buildRobot(RobotType.SCOUT, unitSpawnDirection);
+
+                            }
+                        }
+                    }
+                    else if (needLumberjacks())
+                    {
+                        if (rc.hasRobotBuildRequirements(RobotType.LUMBERJACK))
+                        {
+                            if (rc.canBuildRobot(RobotType.LUMBERJACK, unitSpawnDirection))
+                            {
+                                rc.buildRobot(RobotType.LUMBERJACK, unitSpawnDirection);
+                            }
+                        }
+                    }
+                    else if (needSoldiers())
+                    {
+                        if (rc.hasRobotBuildRequirements(RobotType.SOLDIER))
+                        {
+                            if (rc.canBuildRobot(RobotType.SOLDIER, unitSpawnDirection))
+                            {
+                                rc.buildRobot(RobotType.SOLDIER, unitSpawnDirection);
+                            }
+                        }
+                    }
+
+
+                    visibleAlliedTrees = rc.senseNearbyTrees(1.5f, us);
+                    if (visibleAlliedTrees.length > 0)
+                    {
+                        float minHealth = Float.MAX_VALUE;
+                        int minTreeID = visibleAlliedTrees[0].ID;
+
+                        for (int i = visibleAlliedTrees.length; i-- > 0; )
+                        {
+                            TreeInfo tree = visibleAlliedTrees[i];
+                            if(tree.health < minHealth){
+                                minHealth = tree.health;
+                                minTreeID = tree.ID;
+                            }
+
+                        }
+                        if (rc.canWater(minTreeID))
+                        {
+                            rc.water(minTreeID);
+                        }
+                    }
+                }
+                Clock.yield();
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private static void generateDirections()
+    {
+        treeSpawnDirections = new Direction[5];
+        treeSpawnDirections[0] = HOLE_TOWARDS_ENEMY.rotateLeftDegrees(60);
+        for (int i = 1; i < 5; i++)
+        {
+            treeSpawnDirections[i] = treeSpawnDirections[i - 1].rotateLeftDegrees(60);
+        }
+    }
+
+    private static int determineWanderDuration(int currentRound) throws GameActionException
+    {
+        if (currentRound < 20)
+            return 4;
+        else if (currentRound < 50)
+            return 10;
+        else if (currentRound < 100)
+            return 40;
+        else if (currentRound < 200)
+            return 60;
+        else
+            return 100;
+    }
+
+    private static void callForHelpIfInDangerAndDefend() throws GameActionException
+    {
+        if (visibleEnemies.length > 0)
+        {
+                rc.broadcast(FRIENDLY_GARDENER_UNDER_ATTACK_CHANNEL, 1);
+                rc.broadcast(FRIENDLY_GARDENER_X, Float.floatToIntBits(rc.getLocation().x));
+                rc.broadcast(FRIENDLY_GARDENER_Y, Float.floatToIntBits(rc.getLocation().y));
+                for (RobotInfo robotInfo : visibleEnemies)
+                {
+                    if (robotInfo.getType() == RobotType.SCOUT)
+                    {
+                        spawnInAnyDirectionPossible(RobotType.LUMBERJACK);
+                        break;
+                    }
+                    else
+                    {
+                        spawnInAnyDirectionPossible(RobotType.SOLDIER);
+                        break;
+                    }
+                }
+
+        }
+    }
+
+
+    private static boolean needLumberjacks() throws GameActionException
+    {
+        if (rc.readBroadcast(URGENTLY_NEED_LUMBERJACKS_CHANNEL) == 1)
+        {
+            rc.broadcast(URGENTLY_NEED_LUMBERJACKS_CHANNEL, 0);
+            return true;
+        }
+        return (rc.senseNearbyTrees(-1, Team.NEUTRAL).length > 3);
+    }
+
+    private static boolean spawnInAnyDirectionPossible(RobotType robotType) throws GameActionException
+    {
+        for (int i = 0; i <= 360; i++)
+        {
+            Direction spawnDir = HOLE_TOWARDS_ENEMY.rotateLeftDegrees(i);
+            if (rc.canBuildRobot(robotType, spawnDir))
+            {
+                if (rc.hasRobotBuildRequirements(robotType))
+                {
+                    rc.buildRobot(robotType, spawnDir);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean needScouts() throws GameActionException
+    {
+        return (rc.getRoundNum() < 100 && rc.getTreeCount() > 2);
+
+    }
+
+    public static boolean needSoldiers() throws GameActionException
+    {
+        return true;
+    }
+
+
+    private static int howManyTreesCanBePlanted(MapLocation location)
+    {
+        int howMany = 0;
+        for (Direction d : treeSpawnDirections)
+        {
+            if (rc.canPlantTree(d) && rc.senseNearbyRobots(3.8f, us).length == 0 && rc.senseNearbyTrees(5f).length == 0)
+            {
+                howMany++;
+            }
+        }
+        return howMany;
+    }
+
+    private static void wander() throws GameActionException
+    {
+        if (!rc.hasMoved())
+        {
+            if (!rc.canMove(bounce))
+            {
+                bounce = randomDirection();
+            }
+            else
+                tryMove(bounce);
+        }
+
+    }
+}
---
+++ TheBot/TankBot.java
@@ -0,0 +1,101 @@
+package TheBot;
+
+import battlecode.common.Clock;
+import battlecode.common.Direction;
+import battlecode.common.GameActionException;
+
+public class TankBot extends BaseBot
+{
+
+    private static Direction[] fireDirections = {Direction.getNorth(), Direction.getNorth().rotateRightDegrees(30),Direction.getNorth().rotateRightDegrees(60),
+            Direction.getEast(), Direction.getEast().rotateRightDegrees(30),Direction.getEast().rotateRightDegrees(60), Direction.getSouth(), Direction.getSouth().rotateRightDegrees(30),
+            Direction.getSouth().rotateRightDegrees(60), Direction.getWest(), Direction.getWest().rotateRightDegrees(30), Direction.getWest().rotateRightDegrees(60)};
+
+    private static float senseRadius = rc.getType().sensorRadius;
+    private static Direction bounce = here.directionTo(closetInitalEnemyArchonLocation());
+    private static int enemyArchonNumber=0;
+
+    public static void runTank() throws GameActionException
+    {
+
+        while(true)
+        {
+            try
+            {
+                here = rc.getLocation();
+
+                visibleEnemies = rc.senseNearbyRobots(-1,them);
+                if(visibleEnemies!=null && visibleEnemies.length>0)
+                {
+                    tryMove(here.directionTo(visibleEnemies[0].location));
+                    visibleEnemies = rc.senseNearbyRobots(-1,them);
+                }
+                if(!rc.hasMoved())
+                {
+                    if(enemyArchonNumber>(numberOfInitialArchon-1))
+                        wander();
+                    else if(here.distanceTo(theirInitialArchonLocations[enemyArchonNumber])<myType.bodyRadius)
+                        tryMove(here.directionTo(theirInitialArchonLocations[enemyArchonNumber]));
+                    else
+                        enemyArchonNumber++;
+                }
+
+                if(visibleEnemies.length>0 && rc.canFirePentadShot())
+                {
+                    int fireDirectionIndex = 0,enemiesInDirection;
+                    int maxEnemies = 0;
+                    for(int i=fireDirections.length; i-->0;)
+                    {
+                        enemiesInDirection= rc.senseNearbyRobots(here.add(fireDirections[i],senseRadius/2),senseRadius/2,them).length;
+                        if(enemiesInDirection>maxEnemies)
+                        {
+                            fireDirectionIndex = i;
+                            maxEnemies = enemiesInDirection;
+                        }
+                    }
+                    rc.firePentadShot(fireDirections[fireDirectionIndex]);
+                }
+                else if(visibleEnemies.length>0 && rc.canFireTriadShot())
+                {
+
+//                    int[] heatmap = new int[fireDirections.length];
+                    int fireDirectionIndex = 0,enemiesInDirection;
+                    int maxEnemies = 0;
+                    for(int i=fireDirections.length; i-->0;)
+                    {
+                        enemiesInDirection= rc.senseNearbyRobots(here.add(fireDirections[i],senseRadius/2),senseRadius/2,them).length;
+                        if(enemiesInDirection>maxEnemies)
+                        {
+                            fireDirectionIndex = i;
+                            maxEnemies = enemiesInDirection;
+                        }
+                    }
+                    rc.fireTriadShot(fireDirections[fireDirectionIndex]);
+                }
+
+
+                Clock.yield();
+
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+    }
+    private static void wander() throws GameActionException
+    {
+        if (!rc.hasMoved())
+        {
+            if (!rc.canMove(bounce))
+            {
+                here = rc.getLocation();
+                Direction dir = here.directionTo(closetInitalEnemyArchonLocation());
+                bounce = dir.rotateLeftDegrees((float) ((Math.random() * 180) - 90));
+
+            } else
+                tryMove(bounce);
+        }
+
+    }
+}
